186e25b4289472ccbeceabe5a58c21c8d99da786
PoulasthaMukherjee-NetraLabs <poulastha.mukherjee@netralabs.ai>
2024-11-14 14:21:41 +0530

    Step 1 Completed

--- src/pages/Step1/SubStep2/SubStep2.5.tsx
+++ src/pages/Step1/SubStep2/SubStep2.5.tsx
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { Box, TextField, Button, Typography, Radio, RadioGroup, FormControlLabel } from '@mui/material'; 
+import { Box, Typography, Radio, RadioGroup, FormControlLabel } from '@mui/material'; 
 
 const SubStep2: React.FC = () => { 
   return ( 
@@ -8,55 +8,42 @@ const SubStep2: React.FC = () => {
         @import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap');
       </style>
       <Typography variant="h6" sx={{ mb: 1, fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.85rem', fontWeight: 'bold' }}>
-        <h2>Facility Address</h2>
+        <h2>Other Details</h2>
       </Typography> 
-  
-  <Box sx={{ display: 'flex', gap: 2, width: '100%' }}>
-      
-      {/* Google Maps Column */}
-      <Box sx={{ flex: 1, height: '200px', border: '1px solid lightgrey' }}>
-        <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.086238560343!2d-122.41941548468154!3d37.77492977975966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808c2e5b1bff%3A0xdbf30509b4e22a90!2sGoogle!5e0!3m2!1sen!2sus!4v1666474302084!5m2!1sen!2sus" 
-          width="100%" 
-          height="100%" 
-          style={{ border: 0 }} 
-          allowFullScreen={false} 
-          loading="lazy"
-        ></iframe>
-      </Box>
-      
-      {/* Facility Address Form Column */}
-      <Box sx={{ flex: 1, border: '1px solid lightgrey', p: 1, borderRadius: 1, height: '184.5px' }}>
-      <Typography variant="subtitle2" sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'flex-end' }}>
-          <Button 
-            variant="outlined" 
-            size="small" 
-            sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.7rem', minWidth: '60px', padding: '2px 4px', textTransform: 'none' }}
-          >
-            Confirm Location
-          </Button>
-        </Typography><br />
-        <Box sx={{ fontFamily: 'Nunito Sans, sans-serif', display: 'flex', flexDirection: 'column', gap: 1.5 }}>
-          {["Street Address:", "City:", "State:", "Zip Code:"].map((label, index) => (
-            <Box key={index} sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-              <Typography sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', width: '150px', flex: 0.5 }}><b>{label}</b></Typography>
-              <TextField 
-                variant="outlined" 
-                size="small" 
-                type="text" 
-                sx={{ 
-                  flex: 0.75,
-                  fontSize: '0.7rem', fontFamily: 'Nunito Sans, sans-serif', 
-                  '& .MuiInputBase-root': { height: '24px', padding: '0 6px' },
-                  '& input': { padding: 0 }
-                }} 
-              />
-            </Box>
-          ))}
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 0 }}>
+        <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: 2, p: '10px' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', mt: 1 }}>
+          <Typography variant="subtitle2" sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'space-between' }}>
+          <b><h3>Property Ownership</h3></b> </Typography>
+          <Typography sx={{ mt: 1, fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  Do you own the property?
+  <RadioGroup row sx={{ fontSize: '0.7rem', m: 0 }}>
+    <FormControlLabel value="yes" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.7rem' }}>Yes</Typography>} />
+    <FormControlLabel value="no" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{  fontFamily: 'Nunito Sans, sans-serif',fontSize: '0.7rem' }}>No</Typography>} />
+  </RadioGroup>
+</Typography>
+
+<Typography sx={{ mt: 1,mb: 1, fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+  Do you lease space?
+  <RadioGroup row sx={{ fontSize: '0.7rem', m: 0 }}>
+    <FormControlLabel value="yes" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.7rem' }}>Yes</Typography>} />
+    <FormControlLabel value="no" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{  fontFamily: 'Nunito Sans, sans-serif',fontSize: '0.7rem' }}>No</Typography>} />
+  </RadioGroup>
+</Typography>
+
+<Typography variant="subtitle2" sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'space-between' }}>
+<b><h3>Long - Term Site Occupancy</h3></b> </Typography>
+<Typography sx={{ mt: 1, fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+Do you plan to occupy this site for the next 15-20 years?
+  <RadioGroup row sx={{ fontSize: '0.7rem', m: 0 }}>
+    <FormControlLabel value="yes" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.7rem' }}>Yes</Typography>} />
+    <FormControlLabel value="no" control={<Radio sx={{ padding: '2px' }} />} label={<Typography sx={{  fontFamily: 'Nunito Sans, sans-serif',fontSize: '0.7rem' }}>No</Typography>} />
+  </RadioGroup>
+</Typography>
+          </Box>
         </Box>
       </Box>
-  </Box>
-</Box>
+    </Box>
 
 
   ); };
