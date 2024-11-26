1b3cf7af18bfa0083d325f69de006b3ff769e9bf
PoulasthaMukherjee-NetraLabs <poulastha.mukherjee@netralabs.ai>
2024-11-26 18:35:23 +0530

    aligned map

--- src/pages/Organizational Profile/SubStep2/Facility Address.tsx
+++ src/pages/Organizational Profile/SubStep2/Facility Address.tsx
@@ -11,9 +11,9 @@ const SubStep2: React.FC = () => {
         <h2>Facility Address</h2>
       </Typography> 
   
-  <Box sx={{ display: 'flex', gap: 2, p: '10px' }}>
+  <Box sx={{ display: 'flex', gap: 2, p: '10px', pl: '160px', pr: '160px' }}>
       
-      <Box sx={{ flex: 1, height: '200px', border: '1px solid lightgrey', borderRadius: 1 }}>
+      <Box sx={{ flex: 1, height: '230px', border: '1px solid lightgrey', borderRadius: 1 }}>
         <iframe 
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.086238560343!2d-122.41941548468154!3d37.77492977975966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8085808c2e5b1bff%3A0xdbf30509b4e22a90!2sGoogle!5e0!3m2!1sen!2sus!4v1666474302084!5m2!1sen!2sus" 
           width="100%" 
@@ -24,7 +24,7 @@ const SubStep2: React.FC = () => {
         ></iframe>
       </Box>
       
-      <Box sx={{ flex: 1, border: '1px solid lightgrey', p: 1, borderRadius: 1, height: '184.5px', pl: 2, pr: 2 }}>
+      <Box sx={{ flex: 1, border: '1px solid lightgrey', p: 1, borderRadius: 1, height: '214.5px', pl: 2, pr: 2 }}>
         <Typography variant="subtitle2" sx={{ fontFamily: 'Nunito Sans, sans-serif', fontSize: '0.75rem', display: 'flex', justifyContent: 'flex-end', mt: 0.5 }}>
           <Button 
             variant="outlined" 
