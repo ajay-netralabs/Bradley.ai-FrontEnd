dbe302636592392bbf8b93a4c9dc1a71374f4f08
PoulasthaMukherjee-NetraLabs <poulastha.mukherjee@netralabs.ai>
2024-11-28 17:19:46 +0530

    .

--- src/pages/Onboarding/SubStep1/SubStep1.2.tsx
+++ src/pages/Onboarding/SubStep1/SubStep1.2.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubStep1: React.FC = () => (
   <div>
-    <h2>DER Analysis by Bradley.ai is underway!</h2>
+    <h2>Bradley's Recommendation For Acme Manufacturing</h2>
     {/* <p>This is the content for Step 5, Sub Step 1.</p> */}
   </div>
 );
